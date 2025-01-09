6351cff94161f9526799c9dba89664b223cf779f
ryann0097 <ryandavidrsw@gmail.com>
2025-01-09 18:22:32 -0300

    feat: carregarMaoJogador function with dynamic load

--- src/main/java/br/ufrn/imd/cambio_imd/controllers/GameController.java
+++ src/main/java/br/ufrn/imd/cambio_imd/controllers/GameController.java
@@ -3,7 +3,13 @@ package br.ufrn.imd.cambio_imd.controllers;
 import br.ufrn.imd.cambio_imd.exceptions.UnitializedGameException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
+import javafx.scene.image.*;
+import javafx.scene.layout.GridPane;
+
+import br.ufrn.imd.cambio_imd.models.players.Player;
+import br.ufrn.imd.cambio_imd.models.cards.Card;
+import java.util.Stack;
+
 
 /**
  * Controlador que gerencia a principal view do jogo.
@@ -16,7 +22,8 @@ public class GameController extends ControllerBase {
     private Label drawPileCountLabel;
 
     @FXML
-    private HBox playerCardsHBox;
+    private GridPane playerHandGridPane;
+
 
     // TODO: verificar se essa é uma boa maneira mesmo de renderizar as informações de forma dinâmica
     public void render () {
@@ -24,6 +31,28 @@ public class GameController extends ControllerBase {
             uiManager.renderMain(playerTurnLabel, drawPileCountLabel);
         } catch (UnitializedGameException ex) {
             System.out.println(ex.getMessage());
+        }
+   }
+
+
+    private void carregarMaoJogador(Player player){
+        playerHandGridPane.getChildren().clear();
+        Stack<Card> playerHand = player.getHand().getCards();
+
+        for (int i = 0; i < playerHand.size(); i++) {
+            // Criar um ImageView para cada carta
+            ImageView cardImageView = new ImageView();
+            cardImageView.setFitHeight(81.0);
+            cardImageView.setFitWidth(50.0);
+            cardImageView.setPreserveRatio(true);
+            cardImageView.setPickOnBounds(true);
+
+            // Definir a imagem para o ImageView
+            // String cardImageUrl = playerHand.get(i).getImageUrl();
+            // cardImageView.setImage(new Image(cardImageUrl));
+
+            // Adicionar o ImageView à GridPane na coluna correspondente
+            playerHandGridPane.add(cardImageView, i, 0);
         }
     }
 }

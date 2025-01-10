2dc69e0354d4b51589d0e661a4b1a711e0ba523a
leirdan <uouleirdan@gmail.com>
2025-01-10 09:58:03 -0300

    feat: opções de ação pra uma carta

--- src/main/java/br/ufrn/imd/cambio_imd/controllers/GameController.java
+++ src/main/java/br/ufrn/imd/cambio_imd/controllers/GameController.java
@@ -1,9 +1,10 @@
 package br.ufrn.imd.cambio_imd.controllers;
 
+import br.ufrn.imd.cambio_imd.enums.TransitionType;
 import br.ufrn.imd.cambio_imd.exceptions.UnitializedGameException;
 import br.ufrn.imd.cambio_imd.models.cards.Card;
 import br.ufrn.imd.cambio_imd.utility.CardAssetMapper;
-import javafx.event.EventHandler;
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
@@ -14,8 +15,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 import java.util.Stack;
 
@@ -35,27 +36,49 @@ public class GameController extends ControllerBase {
     @FXML
     private TextArea messageBox;
 
-    private Button playBtn = new Button();
-    private Button swapBtn = new Button();
-    private HBox optionsBox;
+    private final Button playBtn = new Button();
+    private final Button swapBtn = new Button();
+    private VBox optionsBox;
 
     @FXML
     protected void initialize() {
         playBtn.setText("Jogar");
         playBtn.setOnMouseClicked(click -> handlePlayBtnClick());
+        playBtn.setMinWidth(50);
         swapBtn.setText("Trocar");
         swapBtn.setOnMouseClicked(click -> handleSwapBtnClick());
-        optionsBox = new HBox(5, playBtn, swapBtn); // Espaçamento de 5px
+        swapBtn.setMinWidth(50);
+
+        optionsBox = new VBox(5, playBtn, swapBtn);
         optionsBox.setAlignment(Pos.CENTER);
-        optionsBox.setStyle("-fx-background-color: rgba(0, 0, 0, 0.8); -fx-padding: 5px; -fx-border-radius: 5px;");
-        optionsBox.setTranslateY(-50);
+        optionsBox.setStyle("""
+                -fx-background-color: rgba(50, 50, 50, 0.9);
+                -fx-border-color: white;
+                -fx-border-width: 1px;
+                -fx-border-radius: 5px;
+                -fx-background-radius: 5px;
+                -fx-padding: 10px;
+                -fx-margin-bottom: 10px;
+                -fx-effect: dropshadow(gaussian, rgba(0, 0, 0, 0.5), 5, 0, 0, 1);
+               """);
+
+        // Registra ação de fechar a janela de opções caso clique fora
+        playerHandGridPane.addEventFilter(MouseEvent.MOUSE_CLICKED, click -> {
+            Node source = (Node) click.getTarget();
+            boolean clickedOutsideCard = playerHandGridPane.getChildren().stream()
+                    .noneMatch(child -> child == source);
+
+            if (clickedOutsideCard) {
+                applyTransition(optionsBox, Duration.millis(300), TransitionType.FADE_OUT, () -> {
+                    playerHandGridPane.getChildren().remove(optionsBox);
+                });
+            }
+        });
     }
 
 
     public void render() {
         try {
-            // playerTurnLabel.setText(uiManager.getPlayerLabelText());
-            // drawPileCountLabel.setText(uiManager.getDrawPileCountText());
             print("Jogo iniciado");
             renderPlayerHand();
         } catch (UnitializedGameException ex) {
@@ -74,14 +97,11 @@ public class GameController extends ControllerBase {
         Node node = playerHandGridPane.getChildren().get(cardIndex);
         int col = GridPane.getColumnIndex(node);
         int row = GridPane.getRowIndex(node);
-        playerHandGridPane.add(optionsBox, col, row);
+        // Se estiver na 1ª linha aparece em cima da carta, senão, aparece abaixo.
+        optionsBox.setTranslateY(row == 0 ? -50 : 50);
+        applyTransition(optionsBox, Duration.millis(300), TransitionType.FADE_IN);
 
-        // Remove caixa ao clicar fora
-        playerHandGridPane.setOnMouseClicked(click -> {
-            if (click.getSource() != optionsBox){
-                playerHandGridPane.getChildren().remove(optionsBox);
-            }
-        });
+        playerHandGridPane.add(optionsBox, col, row);
     }
 
     protected void handlePlayBtnClick() {
@@ -105,8 +125,7 @@ public class GameController extends ControllerBase {
         final int MAX_COLUMN = 5;
         int correctRow = 0, correctColumn = 0;
 
-        playerHandGridPane.getChildren().clear();
-        // Não é pro controller acessar diretamente os dados do jogo, logo, pedimos ao manager a informação
+        playerHandGridPane.getChildren().clear(); // Limpa as cartas anteriores
         Stack<Card> playerHand = gameManager.getCurrentPlayerCards();
 
         for (int i = 0; i < playerHand.size(); i++) {
@@ -117,7 +136,6 @@ public class GameController extends ControllerBase {
             cardImageView.setPickOnBounds(true);
 
             int index = i; // Precisa disso para não dar erro no parametro abaixo
-            // Verificar porque somente a primeira carta que eu clico exibe a seleção de opções e as outras não..
             cardImageView.setOnMouseClicked(mouseEvent -> {
                 uiManager.setClickedCard(index);
                 handleCardClick(mouseEvent);
@@ -137,5 +155,47 @@ public class GameController extends ControllerBase {
     private void print(String msg) {
         String instant = uiManager.getFormattedInstant();
         messageBox.appendText("[" + instant + "]: " + msg + "\n");
+    }
+
+    /**
+     * Aplica uma transição do tipo <code>FadeTransition</code> em um elemento.
+     * @param element
+     * @param time
+     * @param type
+     */
+    private void applyTransition(Node element, Duration time, TransitionType type) {
+        applyTransition(element, time, type, null);
+    }
+
+    /**
+     * Aplica uma transição do tipo <code>FadeTransition</code> em um elemento.
+     * @param element
+     * @param time
+     * @param type
+     * @param onFinish
+     */
+    private void applyTransition(Node element, Duration time, TransitionType type,
+                                 Runnable onFinish) {
+        FadeTransition transition = new FadeTransition(time, element);
+        switch (type) {
+            case FADE_IN -> {
+                transition.setFromValue(0.0);
+                transition.setToValue(1.0);
+                break;
+            }
+            case FADE_OUT -> {
+                transition.setFromValue(1.0);
+                transition.setToValue(0.0);
+                break;
+            }
+            default -> {
+                break;
+            }
+        }
+
+        if (onFinish != null) {
+            transition.setOnFinished(event -> onFinish.run());
+        }
+        transition.play();
     }
 }
---
+++ src/main/java/br/ufrn/imd/cambio_imd/enums/TransitionType.java
@@ -0,0 +1,6 @@
+package br.ufrn.imd.cambio_imd.enums;
+
+public enum TransitionType {
+    FADE_IN,
+    FADE_OUT
+}

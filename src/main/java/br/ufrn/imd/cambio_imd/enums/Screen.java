1b4bf16dd532d78c90505578c1d91a00e6f3ee2c
leirdan <uouleirdan@gmail.com>
2025-01-11 15:38:28 -0300

    fix: colocando tela certa

--- src/main/java/br/ufrn/imd/cambio_imd/enums/Screen.java
+++ src/main/java/br/ufrn/imd/cambio_imd/enums/Screen.java
@@ -3,7 +3,7 @@ package br.ufrn.imd.cambio_imd.enums;
 public enum Screen {
     MENU("views/screen_cambio_main_menu.fxml"),
     GAME_MODE("views/screen_cambio_game_mode.fxml"),
-    GAME("views/screen_cambio_lightning_match.fxml");
+    GAME("views/screen_cambio_match.fxml");
 
     private final String url;
 

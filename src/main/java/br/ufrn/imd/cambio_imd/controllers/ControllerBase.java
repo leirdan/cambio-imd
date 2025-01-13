a7fef9b42aa6bd7469ac5f827ee0907538bb05d1
ryann0097 <ryandavidrsw@gmail.com>
2025-01-12 23:25:30 -0300

    documenting controllers

--- src/main/java/br/ufrn/imd/cambio_imd/controllers/ControllerBase.java
+++ src/main/java/br/ufrn/imd/cambio_imd/controllers/ControllerBase.java
@@ -10,7 +10,18 @@ import br.ufrn.imd.cambio_imd.managers.ScreenManager;
  * gerenciadores, para que estes tratem dos dados e modifiquem o estado do jogo.</p>
  */
 public abstract class ControllerBase {
+    /**
+     * Atributo que inicializa a classe de gerenciamento do jogo.
+     */
     protected GameManager gameManager = GameManager.getInstance();
+
+    /**
+     * Atributo que inicializa a classe de gerenciamento de telas.
+     */
     protected ScreenManager screenManager = ScreenManager.getInstance();
+
+    /**
+     * Atributo que inicializa a classe de gerenciamento de interface.
+     */
     protected UIManager uiManager = UIManager.getInstance();
 }
--- src/main/java/br/ufrn/imd/cambio_imd/controllers/MenuController.java
+++ src/main/java/br/ufrn/imd/cambio_imd/controllers/MenuController.java
@@ -11,28 +11,62 @@ import javafx.scene.control.Button;
 
 /**
  * Controlador do menu de início e de escolha do modo de jogo.
+ * <p>
+ * Responsável por gerenciar as interações do usuário no menu inicial, incluindo
+ * iniciar o jogo, selecionar o modo de jogo e sair do aplicativo.
+ * </p>
  */
 public class MenuController extends ControllerBase {
+
+    /**
+     * Botão para iniciar o jogo.
+     */
     @FXML
     private Button startBtn;
 
+    /**
+     * Botão para sair do jogo.
+     */
     @FXML
     private Button exitBtn;
 
+    /**
+     * Botão para selecionar o modo de jogo com 6 cartas.
+     */
     @FXML
     private Button sixCardsBtn;
 
+    /**
+     * Botão para selecionar o modo de jogo com 8 cartas.
+     */
     @FXML
     private Button eightCardsBtn;
 
+    /**
+     * Botão para selecionar o modo de jogo com 12 cartas.
+     */
     @FXML
     private Button twelveCardsBtn;
 
+    /**
+     * Manipula o clique no botão "Iniciar".
+     * <p>
+     * Redireciona o usuário para a tela de seleção do modo de jogo.
+     * </p>
+     */
     @FXML
     void handleStartBtnClick() {
         screenManager.change(Screen.GAME_MODE);
     }
 
+    /**
+     * Manipula os cliques nos botões de seleção de modo de jogo.
+     * <p>
+     * Configura o modo de jogo com base no botão clicado, redireciona para a tela de jogo e inicia o jogo.
+     * </p>
+     *
+     * @param event o evento acionado pelo clique no botão de modo de jogo.
+     */
     @FXML
     void handleGameModeBtnClick(ActionEvent event) {
         screenManager.change(Screen.GAME);
@@ -40,6 +74,12 @@ public class MenuController extends ControllerBase {
         gameManager.start();
     }
 
+    /**
+     * Manipula o clique no botão "Sair".
+     * <p>
+     * Encerra a aplicação.
+     * </p>
+     */
     @FXML
     void handleExitBtnClick() {
         Platform.exit();
